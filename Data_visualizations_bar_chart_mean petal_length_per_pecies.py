d1c4ba88bb4490a6b4c7a315f7e25ab0f806e930
ThamXi <ceo@starintellex.com>
2025-11-22 19:43:33 -0800

    GIT: PUSH

---
+++ Data_visualizations_bar_chart_mean petal_length_per_pecies.py
@@ -0,0 +1,6 @@
+plt.figure()
+plt.bar(group_means.index, group_means["petal length (cm)"])
+plt.xlabel("Species")
+plt.ylabel("Mean Petal Length (cm)")
+plt.title("Bar Chart: Average Petal Length per Species")
+plt.show()
